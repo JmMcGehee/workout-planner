30eb2516c4452e5aa9d621ce8e17f320a2f84626
Joshua McGehee <mcgehee.joshua@gmail.com>
2024-05-11 20:12:23 -0700

    tried adding google authentication, rolled it back. created the nav and dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
 
+const MODULES = [
+  CommonModule,
+  RouterOutlet,
+  LoginComponent, 
+  DashboardComponent,
+  NavComponent
+]
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MODULES],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
